882145c3ea1829814ae4308b94f6689e3d1b50bc
danil732000@gmail.com <danil732000@gmail.com>
2019-12-26 16:58:01 +0400

    lab3class

--- WindowsFormsTractor/WindowsFormsTractor/Program.cs
+++ WindowsFormsTractor/WindowsFormsTractor/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsTractor
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FormTractor());
+			Application.Run(new FormParking());
 		}
 	}
 }
