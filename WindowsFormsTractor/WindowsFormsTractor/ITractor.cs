f0c72467e5b252266c56db19fd5d55f9c161cc17
danil732000@gmail.com <danil732000@gmail.com>
2019-12-27 10:16:35 +0400

    lab2home

---
+++ WindowsFormsTractor/WindowsFormsTractor/ITractor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsTractor
+{
+	public interface ITractor
+	{
+		void DrawRinks(TractorRinksCount Count, Graphics g, Color color);
+	}
+}
---
+++ WindowsFormsTractor/WindowsFormsTractor/TractorRinksCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsTractor
+{
+	public enum TractorRinksCount
+	{
+		FOUR = 4,
+		FIVE = 5,
+		SIX = 6
+	}
+}
